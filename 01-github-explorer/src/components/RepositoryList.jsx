dc5e863c9462876618def244b486801b4e794801
Victor Hugo Cordeiro Correa <viktorhugo.dev@gmail.com>
2021-08-25 14:46:07 -0300

    useffect e usestate adicionados

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,23 +1,27 @@
-import { RepositoryItem } from "./RepositoryItem"
-const repository = {
-  name: 'Repo',
-  description: 'forms in React',
-  link: 'https://github.com/ViktorHugodev/reactjs-ignite'
-}
+import { useEffect, useState } from 'react';
+import { RepositoryItem } from './RepositoryItem';
+import '../styles/repositories.scss';
+
+//https://api.github.com/users/viktorhugodev
 
 export function RepositoryList() {
-  return (
-    <section className="repository-list">
-      
-      <h1>Lista de repositórios</h1>
-      
-      <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-      </ul>
-      
-    </section>
-  )
+	const [repositories, setRepositories] = useState([]);
+
+	useEffect(() => {
+		fetch('https://api.github.com/users/ViktorHugodev/repos')
+			.then((response) => response.json())
+			.then((response) => setRepositories(response));
+	}, []);
+
+	return (
+		<section className="repository-list">
+			<h1>Lista de repositórios</h1>
+
+			<ul>
+				{repositories.map(repository => {
+					return <RepositoryItem key={repository.name} repository={repository} />;
+				})}
+			</ul>
+		</section>
+	);
 }
