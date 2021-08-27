cbecc27e1db3f82e91d8b8012fd7cc7994bafbcb
Victor Hugo Cordeiro Correa <viktorhugo.dev@gmail.com>
2021-08-27 17:59:22 -0300

    styled components adicionado

---
+++ 02-dt.money/src/App.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import {GlobalStyle} from './styles/global'
+
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <GlobalStyle/>
+    </div>
+  );
+}
