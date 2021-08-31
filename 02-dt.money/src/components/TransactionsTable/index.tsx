7ae795d29d1aab11d527e47e8a2b18da4a53c348
Victor Hugo Cordeiro Correa <viktorhugo.dev@gmail.com>
2021-08-31 09:01:59 -0300

    configurando e consumindo API

--- 02-dt.money/src/components/TransactionsTable/index.tsx
+++ 02-dt.money/src/components/TransactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import {Container} from './styles'
+import { api } from '../../services/api'
 
 export function Transactions(){
+  useEffect(() => {
+    api.get('transactions')
+    .then(response => console.log(response.data))
+  }, [])
+  
+  
   return (
     <Container>
       <table>
