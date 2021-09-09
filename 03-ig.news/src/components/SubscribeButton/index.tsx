48f0247cca405086572482bb9e7c561da31ea765
Victor Hugo Cordeiro Correa <viktorhugo.dev@gmail.com>
2021-09-09 09:10:52 -0300

    implementado stripe como pagamento

--- 03-ig.news/src/components/SubscribeButton/index.tsx
+++ 03-ig.news/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
+import { signIn, useSession } from 'next-auth/client'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +8,34 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton ({priceId}: SubscribeButtonProps) {
+  const [session] = useSession()
+  
+  async function handleSubscribe() {
+    if (!session){
+      signIn('github')
+      return
+    }
+    
+    try {
+      const response = await api.post('/subscribe')
+      
+      
+      const {sessionId} = response.data
+      
+      const stripe = await getStripeJs()
+      await stripe.redirectToCheckout({sessionId})
+      
+    } catch (error) {
+      console.error(error)
+      alert(error.message)
+    }
+  }
+  
   return (
     <button 
     className={styles.subscribeButton}
-    type="button">
+    type="button"
+    onClick={handleSubscribe}>
       Subscribe now
       </button>
   )
